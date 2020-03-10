072947ee616146c7d5c402381743e42fa67273f9
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-03-11 00:59:24 +0400

    FieldTest checks for the exceptions have been added

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.exceptions.InvalidPointException;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -26,4 +27,60 @@ class FieldTest {
 
         assertEquals(inputFigure, actualFigure);
     }
+
+    @Test
+    void getFigureWhenFigureIsNotSet() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint =new Point(0,0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull( actualFigure);
+    }
+
+    @Test
+    void getFigureWhenXIsLessThenZero() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint =new Point(-1,0);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e){
+
+        }
+    }
+    @Test
+    void getFigureWhenYIsLessThenZero() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint =new Point(0,-1);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e){
+
+        }
+    }
+    @Test
+    void getFigureWhenXIsMoreThenSize() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint =new Point(field.getSize() + 1,0);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e){
+
+        }
+    }
+    @Test
+    void getFigureWhenYIsMoreThenSize() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint =new Point(0,field.getSize() + 1);
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e){
+
+        }
+    }
+
 }
