b2dd838772db31ac41cecfcaf2e74623cf74abe0
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-03-10 23:43:26 +0400

    PlayerTest - created

---
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,34 @@
+package io.hexlet.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getName() throws Exception {
+        final String inputValue = "Andrey";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue,actualValue );
+    }
+
+    @Test
+    void getFigure() throws Exception {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue,actualValue );
+
+    }
+
+}
