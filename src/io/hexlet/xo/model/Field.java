e9d1609ba14fcf2e0f7a7d5f7c79c0bbbceb4d26
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-03-11 00:41:18 +0400

    Field - test for the Point added

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,5 +1,8 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.exceptions.InvalidePointException;
+
 import java.awt.*;
 
 public class Field {
@@ -13,11 +16,20 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidePointException {
+        if(!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidePointException, AlreadyOccupiedException {
+        if(!checkPoint(point)) {
+            throw new InvalidePointException();
+        }
+        if(field[point.x][point.y] != null ) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
     }
     private boolean checkPoint (final Point point) {
