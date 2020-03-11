ce541d5ddc6fd7bae84c239added5d5a3f127f67
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-03-11 21:22:55 +0400

    new version of the WinnerController (recursion)

---
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -0,0 +1,99 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.exceptions.InvalidPointException;
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+
+import java.awt.*;
+
+public class WinnerController {
+
+    public Figure getWinner(final Field field){
+        try {
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(i, 0), p -> new Point(p.x,p.y + 1) )) {
+                    return field.getFigure(new Point(i, 0));
+                }
+            }
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(i, 0), p -> new Point(p.x + 1,p.y) )) {
+                    return field.getFigure(new Point(0, i));
+                }
+            }
+
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1,p.y + 1) )) {
+                return field.getFigure(new Point(0,0));
+            }
+
+
+            if (check(field, new Point(0, 2), p -> new Point(p.x + 1,p.y - 1) )) {
+                return field.getFigure(new Point(0,2));
+            }
+
+//            for (int i = 0; i < 3; i++) {
+//                if (check(field, new Point(i, 0), new IPointGenerator() {
+//                    @Override
+//                    public Point next(Point p) {
+//                        return new Point(p.x,p.y + 1);
+//                    }
+//                })) {
+//                    return field.getFigure(new Point(i, 0));
+//                }
+//            }
+//            for (int i = 0; i < 3; i++) {
+//
+//                if (check(field, new Point(0, i), new IPointGenerator() {
+//                    @Override
+//                    public Point next(Point p) {
+//                        return new Point(p.x + 1,p.y);
+//                    }
+//                })) {
+//                    return field.getFigure(new Point(0, i));
+//                }
+//            }
+//
+//            if(check(field, new Point(0, 0), new IPointGenerator() {
+//                @Override
+//                public Point next(Point p) {
+//                    return new Point(p.x + 1, p.y + 1);
+//                }
+//            })) {
+//                return field.getFigure(new Point(0,0));
+//            }
+//
+//
+//            if(check(field, new Point(0, 2), new IPointGenerator() {
+//                @Override
+//                public Point next(Point p) {
+//                    return new Point(p.x + 1, p.y - 1);
+//                }
+//            })) {
+//                return field.getFigure(new Point(0,2));
+//            }
+
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private boolean check(final Field field, final Point currentPoint, final IPointGenerator pointGenerator){
+        final Figure currentFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
+        try {
+            currentFigure = field.getFigure(currentPoint);
+           nextFigure = field.getFigure(currentPoint);
+        } catch (final InvalidPointException e) {
+            e.printStackTrace();
+            return true;
+        }
+        if (currentFigure == null) { return false; }
+
+        if (currentFigure != nextFigure) { return false; }
+
+        return check(field, nextPoint, pointGenerator);
+    }
+    private interface IPointGenerator {
+        Point next(final Point p);
+    }
+}
