2fd61018c9d1ed49123ea64b3a05a49a2f2cd812
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-03-16 00:06:07 +0400

    Field fix

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -5,38 +5,40 @@ import io.hexlet.xo.exceptions.InvalidPointException;
 import java.awt.*;
 
 public class Field {
-//    private static final int fileSize = 3;
+    //    private static final int fileSize = 3;
     private final int filedSize;
     private static final int MIN_COORDINATE = 0;
-    private final Figure[][] field ;
+    private final Figure[][] field;
 
     public Field(final int fileSize) {
         this.filedSize = fileSize;
         field = new Figure[filedSize][filedSize];
     }
 
-    public int getSize(){
+    public int getSize() {
         return filedSize;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {
-        if(!checkPoint(point)) {
+        if (!checkPoint(point)) {
             throw new InvalidPointException();
         }
         return field[point.x][point.y];
     }
 
     public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
-        if(!checkPoint(point)) {
+        if (!checkPoint(point)) {
             throw new InvalidPointException();
         }
 
         field[point.x][point.y] = figure;
     }
-    private boolean checkPoint (final Point point) {
-        return checkCoordinate(point.x,field.length) && checkCoordinate(point.y, field[point.x].length);
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
-    private boolean checkCoordinate(final int coordinate, final int maxCoordinate ) {
+
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
         return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
     }
 }
